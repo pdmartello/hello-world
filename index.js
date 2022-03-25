894b9f488c2a82130bb6c5f0b15f1272afccfe7d
pdmartello <102316243+pdmartello@users.noreply.github.com>
2022-03-24 22:43:40 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+let count = 5
+count = count + 1
+
+console.log(count)
+
+
+// Create two variables, firstName and lastName
+let firstName = "Trimeric"
+let lastName = "Chimera"
+
+// Concatenate the two variables into a third variable called fullName
+let fullName = firstName + lastName
+
+// Log fullName to the console
+console.log(fullName)
+
